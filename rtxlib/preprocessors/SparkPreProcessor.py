d284899bf5e4e59b6ae7d6ac534aa3058bd552bc
Starofall <starofall@gmail.com>
2017-01-16 04:04:05 +0100

    Fixing Spark kill on Linux

--- rtxlib/preprocessors/SparkPreProcessor.py
+++ rtxlib/preprocessors/SparkPreProcessor.py
@@ -1,8 +1,10 @@
 import atexit
 import os
+import platform
 import subprocess
 import time
 
+import sys
 from colorama import Fore
 
 from rtxlib import info, error
@@ -24,18 +26,26 @@ class SparkPreProcessor(PreProcessor):
             exit(1)
         spark_home = os.environ.get("SPARK_HOME")
         spark_bin = "/bin/spark-submit"
+
+        # http://stackoverflow.com/questions/13243807/popen-waiting-for-child-process-even-when-the-immediate-child-has-terminated/13256908#13256908
+        # set system/version dependent "start_new_session" analogs
+        kwargs = {}
+        if platform.system() == 'Windows':
+            # from msdn [1]
+            CREATE_NEW_PROCESS_GROUP = 0x00000200  # note: could get it from subprocess
+            DETACHED_PROCESS = 0x00000008  # 0x8 | 0x200 == 0x208
+            kwargs.update(creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
+        elif sys.version_info < (3, 2):  # assume posix
+            kwargs.update(preexec_fn=os.setsid)
+        else:  # Python 3.2+ and Unix
+            kwargs.update(start_new_session=True)
         # starting a subprocess to allow termination of spark after we are done
         self.process = subprocess.Popen(spark_home + spark_bin + ' --class ' + self.job_class + \
-                                        ' ./' + wf.folder + '/' + self.job_file, stdout=subprocess.PIPE, shell=True)
+                                        ' ./' + wf.folder + '/' + self.job_file, stdout=subprocess.PIPE, shell=True,**kwargs)
         atexit.register(self.shutdown)
         time.sleep(10)
 
     def shutdown(self):
-        try:
-            # @todo removing the spark processor on windows after running just works this way atm
-            subprocess.Popen("taskkill /f /im java.exe", shell=True)
-        except:
-            pass
         try:
             self.process.kill()
         except:
