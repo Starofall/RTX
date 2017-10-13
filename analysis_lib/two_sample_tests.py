26e599064f2242c7ee70b1aa01939370c92f3a2b
Ilias Gerostathopoulos <iliasger@gmail.com>
2017-10-13 11:14:43 +0200

    two sample tests classes refactoring

---
+++ analysis_lib/two_sample_tests.py
@@ -0,0 +1,62 @@
+from analysis_lib.Analysis import Analysis
+from scipy.stats import ttest_ind
+from math import sqrt, floor
+from numpy import var
+from statsmodels.stats.power import tt_ind_solve_power
+
+class Ttest(Analysis):
+
+    name = "t-test"
+
+    def __init__(self, rtx_run_ids, alpha=0.05):
+        super(self.__class__, self).__init__(rtx_run_ids)
+        self.alpha = alpha
+
+    def run(self, data):
+
+        x1 = [d["overhead"] for d in data[0]]
+        x2 = [d["overhead"] for d in data[1]]
+
+        tstat, pvalue = ttest_ind(x1, x2, equal_var = False)
+
+        different_averages = bool(pvalue <= self.alpha)
+
+        result = dict()
+        result["tstat"] = tstat
+        result["pvalue"] = pvalue
+        result["alpha"] = self.alpha
+        result["different_averages"] = different_averages
+
+        return result
+
+
+class TtestSampleSizeEstimation(Analysis):
+
+    name = "t-test-sample-estimation"
+
+    def __init__(self, rtx_run_ids, mean_diff, alpha=0.05, power=0.8):
+        super(self.__class__, self).__init__(rtx_run_ids)
+        self.alpha = alpha
+        self.power = power
+        self.mean_diff = mean_diff
+
+    def run(self, data):
+
+        x1 = [d["overhead"] for d in data[0]]
+        x2 = [d["overhead"] for d in data[1]]
+
+        pooled_std = sqrt((var(x1) + var(x2)) / 2)
+
+        effect_size = self.mean_diff / pooled_std
+
+        sample_size = tt_ind_solve_power(effect_size=effect_size, nobs1=None, alpha=self.alpha, power=self.power, alternative='two-sided')
+
+        result = dict()
+        result["effect_size"] = effect_size
+        result["sample_size"] = floor(sample_size)
+        result["alpha"] = self.alpha
+        result["power"] = self.power
+        result["mean_diff"] = self.mean_diff
+        result["pooled_std"] = pooled_std
+
+        return result
--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 from RTXRun import RTXRun
 from analysis_lib import setup_database
 
+from analysis_lib.two_sample_tests import Ttest
+from analysis_lib.two_sample_tests import TtestSampleSizeEstimation
 from analysis_lib.normality_tests import AndersonDarling
 from analysis_lib.normality_tests import KolmogorovSmirnov
 from analysis_lib.normality_tests import ShapiroWilk
@@ -14,8 +16,8 @@ if __name__ == '__main__':
     rtx_run_ids.append(RTXRun().start())
     rtx_run_ids.append(RTXRun().start())
 
-    # ttest_analysis = Ttest(rtx_run_ids, alpha=0.05)
-    # ttest_analysis = TtestSampleSizeEstimation(rtx_run_ids, mean_diff=0.1, alpha=0.05, power=0.8)
+    ttest_analysis = Ttest(rtx_run_ids, alpha=0.05).start()
+    ttest_analysis = TtestSampleSizeEstimation(rtx_run_ids, mean_diff=0.1, alpha=0.05, power=0.8).start()
     ttest_analysis = DAgostinoPearson(rtx_run_ids, alpha=0.05).start()
     ttest_analysis = ShapiroWilk(rtx_run_ids, alpha=0.05).start()
     ttest_analysis = AndersonDarling(rtx_run_ids, alpha=0.05).start()
