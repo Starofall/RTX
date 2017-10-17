690dd19d454952673294cbd0e41120fc352182c3
Ilias Gerostathopoulos <iliasger@gmail.com>
2017-10-17 11:40:13 +0200

    equal variances refactoring

--- analysis_lib/equal_variances_tests.py
+++ analysis_lib/equal_variances_tests.py
@@ -3,39 +3,21 @@ from analysis_lib import Analysis
 from scipy.stats import levene
 from scipy.stats import fligner
 from scipy.stats import bartlett
-from rtxlib.rtx_run import get_data_for_run
-from rtxlib import error
 
 
 class EqualVarianceTest(Analysis):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, rtx_run_ids, alpha=0.05):
-        super(EqualVarianceTest, self).__init__(rtx_run_ids)
+    def __init__(self, rtx_run_ids, y_key, alpha=0.05):
+        super(EqualVarianceTest, self).__init__(rtx_run_ids, y_key)
         self.alpha = alpha
-
-    def get_data(self):
-        first_rtx_run_id = self.rtx_run_ids[0]
-        data, exp_count = get_data_for_run(first_rtx_run_id)
-        self.exp_count = exp_count
-
-        for rtx_run_id in self.rtx_run_ids[1:]:
-            new_data, new_exp_count, new_list_of_cofigurations = get_data_for_run(rtx_run_id)
-            for i in range(0,exp_count):
-                data[i] += new_data[i]
-
-        if not data:
-            error("Tried to run analysis on empty data. Aborting.")
-            return
-
-        return data
 
     def run(self, data):
 
         y = []
         for i in range(0, self.exp_count):
-            y.append([d["overhead"] for d in data[i]])
+            y.append([d[self.y_key] for d in data[i]])
 
         statistic, pvalue = self.get_statistic_and_pvalue(y)
 
