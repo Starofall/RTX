2d732b511673fe4aa6b72b2af89ba21e65e63644
Ilias Gerostathopoulos <iliasger@gmail.com>
2018-01-15 12:23:07 +0100

    updating factorial analysis helper script

--- factorial_anova_analysis.py
+++ factorial_anova_analysis.py
@@ -1,53 +1,20 @@
 from rtxlib.rtx_run import setup_database, db
 from analysis_lib.factorial_tests import FactorialAnova
+from parallel import get_experiment_list
+from statsmodels.graphics.factorplots import interaction_plot
 
-index = "rtx752"   # dual of 750
-file_name = index + ".pickle"
-
-knob_names = [
-    "route_random_sigma",
-    "exploration_percentage",
-    # "max_speed_and_length_factor",
-    # "average_edge_duration_factor",
-    # "freshness_update_factor",
-    # "freshness_cut_off_value",
-    # "re_route_every_ticks",
-]
-
-knob_values = [
-    [0, 0.2, 0.4, 0.6],   # route_random_sigma
-    [0, 0.2, 0.4, 0.6],   # exploration_percentage
-    [1, 1.5, 2, 2.5],     # max_speed_and_length_factor
-    [1, 1.5, 2, 2.5],     # average_edge_duration_factor
-    [5, 10, 15, 20],      # freshness_update_factor
-    [100, 300, 500, 700], # freshness_cut_off_value
-    [10, 30, 50, 70]      # re_route_every_ticks"
-]
-
-default_dict = {
-    "route_random_sigma" : 0,
-    "exploration_percentage": 0,
-    "max_speed_and_length_factor": 1,
-    "average_edge_duration_factor": 1,
-    "freshness_update_factor": 10,
-    "freshness_cut_off_value": 90,
-    "re_route_every_ticks": 60
+strategy_knobs = {
+    "route_random_sigma": ([0.0, 0.3], 0.3),
+    "exploration_percentage": ([0.0, 0.3], 0.3),
+    "max_speed_and_length_factor": ([1.0, 2.5], 1.5),
+    "average_edge_duration_factor": ([1.0, 2.5], 1.5),
+    "freshness_update_factor": ([5, 20], 15),
+    "freshness_cut_off_value": ([100, 700], 600),
+    "re_route_every_ticks": ([10, 70], 60),
 }
 
 
-def get_knob_keys():
-    knob_keys = []
-    knob_name_ind = 0
-    for knob_name in knob_names:
-        for knob_value in knob_values[knob_name_ind]:
-            knob_key = {}
-            knob_key[knob_name] = knob_value
-            knob_keys.append(knob_key)
-        knob_name_ind += 1
-    return knob_keys
-
-
-def save_dict_to_file(all_complaints):
+def save_dict_to_file(all_complaints, file_name):
     import pickle
     pickle_out = open(file_name,"wb")
     pickle.dump(all_complaints, pickle_out)
@@ -55,7 +22,7 @@ def save_dict_to_file(all_complaints):
     print "data saved to file " + file_name
 
 
-def retrieve_dict_from_file():
+def retrieve_dict_from_file(file_name):
     import pickle
     pickle_in = open(file_name,"rb")
     all_complaints = pickle.load(pickle_in)
@@ -63,60 +30,166 @@ def retrieve_dict_from_file():
     return all_complaints
 
 
-def get_raw_data(from_server):
+def get_raw_data(index, from_server, sample_size):
+    file_name = index + "-" + str(sample_size) + ".pickle"
     if from_server:
         setup_database(index)
         res = db().get_all_data_points()
-        save_dict_to_file(res)
-        return res
+        save_dict_to_file(res, file_name)
     else:
-        res = retrieve_dict_from_file()
-        return res
+        res = retrieve_dict_from_file(file_name)
+    return res
 
 
-def get_full_knobs_dict(retrieved_dict):
-    full_dict = {}
-    for knob_name in knob_names:
-        if knob_name in retrieved_dict:
-            full_dict[knob_name] = retrieved_dict[knob_name]
-        else:
-            full_dict[knob_name] = default_dict[knob_name]
-    return full_dict
+def create_experiment_configuration(knobs_keys, knobs_values):
+    exp_conf = {}
+    for i in range(len(knobs_keys)):
+        exp_conf[knobs_keys[i]] = knobs_values[i]
+    return exp_conf
 
-def get_data_and_knobs(from_server, sample_size):
-    results = get_raw_data(from_server)
 
+def get_data_and_knobs(index, strategy_knobs, from_server, sample_size):
+    results = get_raw_data(index, from_server, sample_size)
+
+    exp_list = get_experiment_list("step_explorer", strategy_knobs)
+
+    cnt = 0
     data = {}
     knobs = {}
 
-    cnt = 0
     print "~~~~~~~~~"
     print index
     print "~~~~~~~~~"
-    for j in range(len(knob_names)):
-        knob_name = knob_names[j]
-        print knob_name
-        print "---------"
-        for i in knob_values[j]:
-            res = [r for r in results if r[1].get(knob_name)==i]
-            # ind = knob_name + "_" + str(i)
-            data[cnt] = [r[0] for r in res][:sample_size]
-            knobs[cnt] = [get_full_knobs_dict(r[1]) for r in res][:sample_size]
-            print str(i)+"'s representative knob: " + str(knobs[cnt][0])
-            print str(i)+"'s data: " + str(len(data[cnt]))
-            cnt += 1
-        print "========="
 
-    return data, knobs
+    for knob_values in exp_list:
+        exp_conf = create_experiment_configuration(strategy_knobs.keys(), knob_values)
+
+        # exp_conf1 = exp_conf.copy()
+        # exp_conf1["route_random_sigma"] = 0.0
+        # exp_conf2 = exp_conf.copy()
+        # exp_conf2["route_random_sigma"] = 0.3
+
+        # res = [r for r in results if r[1]==exp_conf1 or r[1]==exp_conf2]
+        res = [r for r in results if r[1]==exp_conf]
+        data[cnt] = [r[0] for r in res][:sample_size]
+        knobs[cnt] = [r[1] for r in res][:sample_size]
+        # print exp_conf
+        # print "Data: " + str(len(data[cnt]))
+        cnt += 1
+    print "========="
+
+    return data, knobs, cnt
+
+
+def make_data_smaller(index, strategy_knobs):
+
+    results = get_raw_data(index, False, 10000)
+
+    exp_list = get_experiment_list("step_explorer", strategy_knobs)
+
+    print "~~~~~~~~~"
+    print index
+    print "~~~~~~~~~"
+
+    new_results = []
+    for knob_values in exp_list:
+        exp_conf = create_experiment_configuration(strategy_knobs.keys(), knob_values)
+        res = [r for r in results if r[1]==exp_conf]
+        print exp_conf
+        print "res: " + str(len(res))
+        new_res = res[:1000]
+        print "new_res: " + str(len(new_res))
+        new_results += new_res
+    print "========="
+    print "new_results: " + str(len(new_results))
+
+    file_name = index + "-1000.pickle"
+    save_dict_to_file(new_results, file_name)
 
 
 if __name__ == '__main__':
 
-    data, knobs = get_data_and_knobs(False, 30000)
+    # index = "rtxfactorial1"      # 750 cars, 750 smart cars
+    # index = "rtxfactorial350"    # 750 cars, 350 smart cars
+    index = "rtxfactorial350350" # 350 cars, 350 smart cars
+
+    data, knobs, exp_count = get_data_and_knobs(index, strategy_knobs, True, 1000)
+    print "exp_count: " + str(exp_count)
+
+    # make_data_smaller(index, strategy_knobs)
+
 
     fake_run_id = "123456"
     y_key = 'overhead'
-    exp_count = len(knob_names) * len(knob_values[0])
-    print "exp_count: " + str(exp_count)
 
-    FactorialAnova(fake_run_id, y_key, knob_names, exp_count).start(data, knobs)
+    aov_table = FactorialAnova(fake_run_id, y_key, strategy_knobs.keys(), exp_count).start(data, knobs)
+    save_dict_to_file(aov_table, index + "_res.pickle")
+    print "-------------"
+
+
+
+
+
+    # if __name__ == '__main__':
+    # index = "rtxfactorial1"
+    # aov_table = retrieve_dict_from_file(index + "_res.pickle")
+    # aov_table = aov_table.sort_values(by='PR(>F)', ascending=True)
+    # probabilities = aov_table["PR(>F)"]
+    #
+    # significant_probs =  probabilities[probabilities < 0.01]
+    #
+    # print significant_probs
+    #
+    # print "Evaluated " + str(len(probabilities)) + " out of which " + str(len(significant_probs)) + " were found significant."
+
+    # fig = interaction_plot(data.dose, data.supp, data.len, ms=10)
+
+    # knob_names = [
+    #     "route_random_sigma",
+    #     "exploration_percentage",
+    #     "max_speed_and_length_factor",
+    #     "average_edge_duration_factor",
+    #     "freshness_update_factor",
+    #     "freshness_cut_off_value",
+    #     "re_route_every_ticks",
+    # ]
+    #
+    # knob_values = [
+    #     [0, 0.3],          # route_random_sigma
+    #     [0, 0.3],          # exploration_percentage
+    #     [1, 2.5],          # max_speed_and_length_factor
+    #     [1, 2.5],          # average_edge_duration_factor
+    #     [5, 20],           # freshness_update_factor
+    #     [100, 700],        # freshness_cut_off_value
+    #     [10, 70]           # re_route_every_ticks"
+    # ]
+    #
+    # default_dict = {
+    #     "route_random_sigma" : 0,
+    #     "exploration_percentage": 0,
+    #     "max_speed_and_length_factor": 1,
+    #     "average_edge_duration_factor": 1,
+    #     "freshness_update_factor": 5,
+    #     "freshness_cut_off_value": 100,
+    #     "re_route_every_ticks": 10
+    # }
+
+    # def get_knob_keys():
+    #     knob_keys = []
+    #     knob_name_ind = 0
+    #     for knob_name in knob_names:
+    #         for knob_value in knob_values[knob_name_ind]:
+    #             knob_key = {}
+    #             knob_key[knob_name] = knob_value
+    #             knob_keys.append(knob_key)
+    #         knob_name_ind += 1
+    #     return knob_keys
+
+    # def get_full_knobs_dict(retrieved_dict):
+    #     full_dict = {}
+    #     for knob_name in knob_names:
+    #         if knob_name in retrieved_dict:
+    #             full_dict[knob_name] = retrieved_dict[knob_name]
+    #         else:
+    #             full_dict[knob_name] = default_dict[knob_name]
+    #     return full_dict
